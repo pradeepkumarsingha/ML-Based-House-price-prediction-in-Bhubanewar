7219230cec1e5fb8cb471e15033db9f38f88fd52
Pradeep Kumar Singha <mr.pradeepkumarsingha@gmail.com>
2026-03-16 00:22:17 +0530

    backend success fully deployed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-});
+// const API = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL
+// });
+const API="https://your-app-name.onrender.com/predict"
 
 export const getPrediction = (data) => API.post('/houses/predict', data);
